14d02d9e45dc9564f4eb02c21d3244b0d24d899d
Chris Alfano <chris@jarv.us>
2020-03-28 02:45:35 +0000

    test: finish Organization.loadProjects tests

--- crawler/lib/parsers/__tests__/Organization.test.js
+++ crawler/lib/parsers/__tests__/Organization.test.js
@@ -1,10 +1,31 @@
 const Organization = require('../Organization.js');
+const GitHubOrganization = require('../../repositories/projects/GitHubOrganization.js');
+const GitHubTopic = require('../../repositories/projects/GitHubTopic.js');
+const File = require('../../repositories/projects/File.js');
+const Projects = require('../../repositories/Projects.js');
 
 describe('Organization Parser', () => {
     test('load projects from GitHub organization', async () => {
-        const result = await Organization.loadProjects({
+        const projects = await Organization.loadProjects({
             projects_list_url: 'https://github.com/codeforabq'
         });
-        debugger;
+        expect(projects).toBeInstanceOf(GitHubOrganization);
+        expect(projects).toBeInstanceOf(Projects);
+    });
+
+    test('load projects from GitHub topic', async () => {
+        const projects = await Organization.loadProjects({
+            projects_list_url: 'https://github.com/topics/hack-for-la'
+        });
+        expect(projects).toBeInstanceOf(GitHubTopic);
+        expect(projects).toBeInstanceOf(Projects);
+    });
+
+    test('load projects from file', async () => {
+        const projects = await Organization.loadProjects({
+            projects_list_url: 'https://raw.github.com/open-city/civic-json-files/master/projects.json'
+        });
+        expect(projects).toBeInstanceOf(File);
+        expect(projects).toBeInstanceOf(Projects);
     });
 });
