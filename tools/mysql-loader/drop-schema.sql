4f1101251155e1d4806c4f2d5d641e85e0b38025
Giovanni Sce <giosce@verizon.net>
2021-06-19 19:24:26 -0400

    mysql scripts.

---
+++ tools/mysql-loader/drop-schema.sql
@@ -0,0 +1,20 @@
+USE `cfa_project_index`;
+DROP TABLE `organizations_locations`;
+DROP TABLE `organizations_tags`;
+DROP TABLE `locations`;
+
+DROP TABLE `organizations`;
+DROP TABLE `tags`;
+
+DROP TABLE `projects_topics`;
+DROP TABLE `projects`;
+DROP TABLE `topics`;
+
+DROP TABLE `periods`;
+DROP TABLE `states_regions`;
+
+DROP VIEW `brigades_topics`;
+DROP VIEW `projects_last_pushed_within`;
+DROP VIEW `organizations_last_pushed_within`;
+DROP VIEW `organizations_projects_count`;
+DROP VIEW `projects_organizations_locations`;
