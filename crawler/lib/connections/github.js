91a73287880f3604fd401b2ee1a5fe9fa9ff6556
Chris Alfano <chris@jarv.us>
2022-12-13 20:23:38 -0500

    fix(github): implement handling of soft rate limits

--- crawler/lib/connections/github.js
+++ crawler/lib/connections/github.js
@@ -1,8 +1,9 @@
 const axios = require('axios');
+const axiosRetryAfter = require('axios-retry-after');
 
 const { GITHUB_ACTOR: githubActor, GITHUB_TOKEN: githubToken } = process.env;
 
-module.exports = axios.create({
+const axiosClient = axios.create({
     baseURL: 'https://api.github.com',
     headers: {
         Accept: 'application/vnd.github.mercy-preview+json'
@@ -11,3 +12,21 @@ module.exports = axios.create({
         ? { username: githubActor, password: githubToken }
         : null
 });
+
+axiosClient.interceptors.response.use(null, axiosRetryAfter(axiosClient, {
+    // GitHub responds with 403 instead of 429 when soft rate limit is hit
+    isRetryable (error) {
+        return (
+            error.response && error.response.status === 403 &&
+            error.response.headers['retry-after']
+        );
+    },
+
+    wait (error) {
+        return new Promise(
+            resolve => setTimeout(resolve, error.response.headers['retry-after'] * 1000)
+        );
+    }
+}));
+
+module.exports = axiosClient;
